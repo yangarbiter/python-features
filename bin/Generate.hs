0a3fa9abb046d2006677b5c8cfdd662bae40e0b6
Benjamin Cosman <bcosman@caltech.edu>
2018-02-12 09:54:17 -0800

    removed unused funcs in Generate.hs

--- bin/Generate.hs
+++ bin/Generate.hs
@@ -117,31 +117,6 @@ mkBadFeaturesWithSlice withSlice out nm fs jsons = do
     -- LBSC.writeFile path $ encodeByName (head header) (concat features)
   printf "MEAN / STD frac: %.3f / %.3f\n" mean std
 
-mkFixFeatures :: String -> [Feature] -> [String] -> IO ()
-mkFixFeatures nm fs jsons = do
-  let fixes = concatMap mkFixes jsons
-  let (header, features) = unzip $ map (runTFeaturesTypes fs) fixes
-  let path = "data/" ++ nm ++ ".csv"
-  LBSC.writeFile path $ encodeByName (head header) (concat features)
-
-
-traceStats :: [[NamedRecord]] -> IO ()
-traceStats outss = do
-  let fracs = [ fromIntegral (length (filter (\out -> out HashMap.! "L-DidChange" == "1.0") outs)) / fromIntegral (length outs)
-              | outs <- outss
-              , length outs > 0
-              ] :: [Double]
-  hPrintf stderr "%s\n" (show fracs)
-  hPrintf stderr "Min: %f\n" (minimum fracs)
-  hPrintf stderr "Max: %f\n" (maximum fracs)
-  let avg = sum fracs / fromIntegral (length fracs)
-  hPrintf stderr "Avg: %f\n" avg
-  let std = sqrt (sum [(f - avg) ^ 2 | f <- fracs] / fromIntegral (length fracs))
-  hPrintf stderr "Std: %f\n" std
-  return ()
-
--- uniqDiffs :: [String] -> HashSet ([SrcSpan], Prog, String, String, Int)
--- uniqDiffs = foldl' (\seen json -> seen `mappend` HashSet.fromList (mkDiffs json)) mempty
 
 parseTopForm :: Int -> String -> Either String [StatementSpan]
 parseTopForm v code = case (parseModule v) code "foo.py" of
@@ -189,25 +164,7 @@ mkProgs json = case eitherDecode (LBSC.pack json) of
     , Right bad <- parseTopForm v bad'
     -> Right (bad, fix) -- [(ss, bad, bad', fix')]
 
-mkFixes :: String -> [Prog]
-mkFixes json = case eitherDecode (LBSC.pack json) of
-  Left e -> {-trace e-} mempty
-  Right (MkInSample bad' fix' v _ _)
-  --Right (MkInSample bads' (fix':_))
-    | Right fix <- parseTopForm v fix'
-    -> [fix]
-    -- -> HashSet.fromList . maybeToList $ mkDiff fix bad
-  Right (MkInSample bad' fix' v _ _)
-  --Right (MkInSample bads' (fix':_))
-    | Left e <- parseTopForm v fix'
-    -> {-trace e-} error "f1"
-  Right (MkInSample bad' fix' v _ _)
-  --Right (MkInSample bads' (fix':_))
-    | Left e <- parseTopForm v bad'
-    -> {-trace e-} error "f2"
-  x -> error (show x)
-
-mkDiff'' :: Prog -> Prog -> [SrcSpan] --TODO currently returns []
+mkDiff'' :: Prog -> Prog -> [SrcSpan] --TODO currently returns []?
 mkDiff'' bad fix
   --- | null x
   -- = trace (render $ prettyProg bad) $ trace (render $ prettyProg fix) $ trace "" $ undefined
@@ -261,21 +218,6 @@ boolToDouble :: Bool -> Double
 boolToDouble True = 1
 boolToDouble False = 0
 
-runTFeaturesTypes
-  :: [Feature] -> Prog
-  -> (Header, [NamedRecord])
-runTFeaturesTypes fs fix = (header, samples)
-  where
-  header = Vector.fromList
-         $ concatMap (\(ls,_) -> map mkFeature ls) fs
-
-  samples = concatMap mkTypeOut fix
-
-  mkTypeOut :: StatementSpan -> [NamedRecord]
-  mkTypeOut te = ctfold f [] (St te)
-    where
-    f p e acc = (:acc) . namedRecord $
-                concatMap (\(ls,c) -> zipWith (.=) (map mkFeature ls) (c p e)) fs
 
 mkLabel :: String -> BSC.ByteString
 mkLabel s = BSC.pack ("L-" ++ s)
