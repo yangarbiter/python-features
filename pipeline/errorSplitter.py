1098ecad1afe36e5c050591bc1a97c9e2944f49e
Benjamin Cosman <bcosman@caltech.edu>
2018-08-02 20:55:51 -0700

    data pipeline

---
+++ pipeline/errorSplitter.py
@@ -0,0 +1,16 @@
+import os, sys, json, csv
+
+dataDir = sys.argv[1]
+
+inDir = os.path.join(dataDir, 'outliersRemoved')
+outDir = os.path.join(dataDir, 'subsets')
+
+for filename in os.listdir(inDir):
+    src = os.path.join(inDir, filename)
+    with open(src) as f:
+        x = csv.DictReader(f)
+        err = next(x)['F-PythonMsg']
+        errDir = os.path.join(outDir, err)
+        if not os.path.exists(errDir):
+            os.mkdir(errDir)
+        os.link(src, os.path.join(errDir, filename))
